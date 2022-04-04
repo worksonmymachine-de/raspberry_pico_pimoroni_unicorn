81b01a333f0a2c1655485b685ec0c04703f0e726
worksonmymachine <jazzfreund@gmx.de>
2022-04-04 09:22:32 +0200

    Update picounicorn.py

--- picounicorn.py
+++ picounicorn.py
@@ -1,17 +1,17 @@
 # Dummy implementation of the picounicorn API with some optional debug and test features.
 
-__init_called = False
-__WIDTH = 16
-__HEIGHT = 7
-
 # Toggle any print statements for debugging here or use dummy_logging_off / dummy_logging_on
 # (don't forget to remove the method calls to dummy* from your code before deploying on Pico)
 # Default it is turned off. When enabled it prints out which rbg color at which (LED )coords
 # was set and if you did call init or not - or multiple times.
 # Button pressed states (A is pressed) can be simulated by dummy_set_button_state(BUTTON_A, True)
-# Every single button can be simulated and the dummy_reset_buttons() method can be used to reset 
+# Every single button can be simulated and the dummy_reset_buttons() method can be used to reset
 # all buttons to False (e.g. in unit tests)
-__logging = False
+
+__init_called = False
+__WIDTH = 16
+__HEIGHT = 7
+__logging_on = False
 
 BUTTON_A = "A"
 BUTTON_B = "B"
@@ -24,6 +24,7 @@ buttons = {
     BUTTON_X: False,
     BUTTON_Y: False,
 }
+
 
 def init():
     global __init_called
@@ -58,29 +59,34 @@ def set_pixel_value(x: int, y: int, v: int) -> None:
     __ensure_init_called()
     if __logging():
         print(f"picounicorn.set_pixel_value() called: x={x}, y={y}, v={v}")
-        
-def is_pressed(buttonKey: String) -> bool:
-    return buttons.get(buttonKey)
+
+
+def is_pressed(button_key: str) -> bool:
+    return buttons.get(button_key)
+
 
 def __logging() -> bool:
-    return __logging
+    return __logging_on
+
 
 def dummy_logging_off():
-    global __logging
-    __logging = False
+    global __logging_on
+    __logging_on = False
 
 
 def dummy_logging_on():
-    global __logging
-    __logging = True
-    
-def dummy_set_button_state(buttonKey: String, state: bool) -> None:
+    global __logging_on
+    __logging_on = True
+
+
+def dummy_set_button_state(button_key: str, state: bool) -> None:
     if __logging():
-        print(f"setting button {buttonKey} to pressed state {state}")
-    buttons.update({buttonKey: state})
-    
+        print(f"setting button {button_key} to pressed state {state}")
+    buttons.update({button_key: state})
+
+
 def dummy_reset_buttons() -> None:
     if __logging():
         print("resetting all buttons to pressed state False")
-    {buttons.update({key: False}) for key in buttons.keys()}
-
+    for key in buttons.keys():
+        buttons.update({key: False})
