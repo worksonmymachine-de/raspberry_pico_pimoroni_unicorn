2d4682ad7d010567fc987a5bdba8b65e5d83490e
worksonmymachine <jazzfreund@gmx.de>
2022-04-04 11:24:22 +0200

    corrected naming
    using _ instead of __

--- picounicorn.py
+++ picounicorn.py
@@ -8,17 +8,17 @@
 # Every single button can be simulated and the dummy_reset_buttons() method can be used to reset
 # all buttons to False (e.g. in unit tests)
 
-__init_called = False
-__WIDTH = 16
-__HEIGHT = 7
-__logging_on = False
+_init_called = False
+_WIDTH = 16
+_HEIGHT = 7
+_logging_on = False
 
 BUTTON_A = "A"
 BUTTON_B = "B"
 BUTTON_X = "X"
 BUTTON_Y = "Y"
 
-buttons = {
+_buttons = {
     BUTTON_A: False,
     BUTTON_B: False,
     BUTTON_X: False,
@@ -27,66 +27,66 @@ buttons = {
 
 
 def init():
-    global __init_called
-    if not __init_called:
-        __init_called = True
-    elif __init_called and __logging():
+    global _init_called
+    if not _init_called:
+        _init_called = True
+    elif _init_called and _logging():
         print("picounicorn.init() was already called!")
 
 
-def __ensure_init_called() -> None:
-    if not __init_called and __logging():
+def _ensure_init_called() -> None:
+    if not _init_called and _logging():
         print("Call picounicorn.init() first!")
 
 
 def get_width() -> int:
-    __ensure_init_called()
-    return __WIDTH
+    _ensure_init_called()
+    return _WIDTH
 
 
 def get_height() -> int:
-    __ensure_init_called()
-    return __HEIGHT
+    _ensure_init_called()
+    return _HEIGHT
 
 
 def set_pixel(x: int, y: int, r: int, b: int, g: int) -> None:
-    __ensure_init_called()
-    if __logging():
+    _ensure_init_called()
+    if _logging():
         print(f"picounicorn.set_pixel() called: x={x}, y={y}, rgb={r},{g},{b}")
 
 
 def set_pixel_value(x: int, y: int, v: int) -> None:
-    __ensure_init_called()
-    if __logging():
+    _ensure_init_called()
+    if _logging():
         print(f"picounicorn.set_pixel_value() called: x={x}, y={y}, v={v}")
 
 
 def is_pressed(button_key: str) -> bool:
-    return buttons.get(button_key)
+    return _buttons.get(button_key)
 
 
-def __logging() -> bool:
-    return __logging_on
+def _logging() -> bool:
+    return _logging_on
 
 
 def dummy_logging_off():
-    global __logging_on
-    __logging_on = False
+    global _logging_on
+    _logging_on = False
 
 
 def dummy_logging_on():
-    global __logging_on
-    __logging_on = True
+    global _logging_on
+    _logging_on = True
 
 
 def dummy_set_button_state(button_key: str, state: bool) -> None:
-    if __logging():
+    if _logging():
         print(f"setting button {button_key} to pressed state {state}")
-    buttons.update({button_key: state})
+    _buttons.update({button_key: state})
 
 
 def dummy_reset_buttons() -> None:
-    if __logging():
+    if _logging():
         print("resetting all buttons to pressed state False")
-    for key in buttons.keys():
-        buttons.update({key: False})
+    for key in _buttons.keys():
+        _buttons.update({key: False})
