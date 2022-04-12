0c89945988c32eb8eaf2da359a5f8438f5f0a033
worksonmymachine <jazzfreund@gmx.de>
2022-04-12 10:59:37 +0200

    Added coords and rgb checks, improved logging
    All debugging and check methods are now disabled when logging is disabled (default). This way the dummy just provides the methods of your unicorn.
    
    Added checks and logging messages if coordinates and rgb values are valid.

--- picounicorn.py
+++ picounicorn.py
@@ -13,6 +13,10 @@ _HEIGHT = 7
 _init_called = False
 _logging_on = False
 
+_x_range = (0, 6)
+_y_range = (0, 15)
+_rgb_range = (0, 255)
+
 BUTTON_A = "A"
 BUTTON_B = "B"
 BUTTON_X = "X"
@@ -27,32 +31,51 @@ _buttons = {
 
 
 def init():
-    global _init_called
-    if _init_called:
-        _log("picounicorn.init() was already called!")
-    else:
-        _init_called = True
-        _log("picounicorn.init() called")
+    if _logging():
+        global _init_called
+        if _init_called:
+            _log("picounicorn.init() was already called!")
+        else:
+            _init_called = True
+            _log("picounicorn.init() called")
 
 
 def get_width() -> int:
-    _ensure_init_called()
+    if _logging():
+        _ensure_init_called()
     return _WIDTH
 
 
 def get_height() -> int:
-    _ensure_init_called()
+    if _logging():
+        _ensure_init_called()
     return _HEIGHT
 
 
 def set_pixel(x: int, y: int, r: int, b: int, g: int) -> None:
-    _ensure_init_called()
-    _log(f"picounicorn.set_pixel() called: coords={x},{y} rgb={r},{g},{b}")
+    if _logging():
+        _ensure_init_called()
+        coords_valid = _coords_valid(x, y)
+        rgb_values_valid = _rgb_values_valid(r, g, b)
+        if coords_valid and rgb_values_valid:
+            _log(f"picounicorn.set_pixel() called: coords={x},{y} rgb={r},{g},{b}")
+        elif not coords_valid:
+            _log(f"ERROR: coordinates {x},{y} invalid - should be in range x:{_x_range} and y:{_y_range}")
+        elif not rgb_values_valid:
+            _log(f"ERROR: rgb values {r},{g},{b} invalid - should be in range {_rgb_range}")
 
 
 def set_pixel_value(x: int, y: int, v: int) -> None:
-    _ensure_init_called()
-    _log(f"picounicorn.set_pixel_value() called: coords={x},{y} v={v}")
+    if _logging():
+        _ensure_init_called()
+        coords_valid = _coords_valid(x, y)
+        rgb_value_valid = _rgb_value_valid(v)
+        if coords_valid and rgb_value_valid:
+            _log(f"picounicorn.set_pixel_value() called: coords={x},{y} v={v}")
+        elif not coords_valid:
+            _log(f"ERROR: coordinates {x},{y} invalid - should be in range x:{_x_range} and y:{_y_range}")
+        elif not rgb_value_valid:
+            _log(f"ERROR: rgb value {v} invalid - should be in range {_rgb_range}")
 
 
 def is_pressed(button_key: str) -> bool:
@@ -65,12 +88,23 @@ def _logging() -> bool:
 
 def _ensure_init_called() -> None:
     if not _init_called:
-        _log("Call picounicorn.init() first!")
+        _log("ERROR: Call picounicorn.init() first!")
+
+
+def _coords_valid(x: int, y: int) -> bool:
+    return x in _x_range and y in _y_range
+
+
+def _rgb_values_valid(r: int, g: int, b: int) -> bool:
+    return _rgb_value_valid(r) and _rgb_value_valid(g) and _rgb_value_valid(b)
+
+
+def _rgb_value_valid(v: int) -> bool:
+    return v in _rgb_range
 
 
 def _log(msg: str) -> None:
-    if _logging():
-        print(msg)
+    print(msg)
 
 
 def dummy_logging_off():
@@ -84,11 +118,13 @@ def dummy_logging_on():
 
 
 def dummy_set_button_state(button_key: str, state: bool) -> None:
-    _log(f"setting button {button_key} to pressed state {state}")
+    if _logging():
+        _log(f"setting button {button_key} to pressed state {state}")
     _buttons.update({button_key: state})
 
 
 def dummy_reset_buttons() -> None:
-    _log("resetting all buttons to pressed state False")
+    if _logging():
+        _log("resetting all buttons to pressed state False")
     for key in _buttons.keys():
         _buttons.update({key: False})
