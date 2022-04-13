20922204a4dcd0783e360ace94760e5976924a5e
worksonmymachine <jazzfreund@gmx.de>
2022-04-13 12:16:33 +0200

    Create uasyncio.py

---
+++ uasyncio.py
@@ -0,0 +1,16 @@
+# Quick dummy implementation of uasyncio methods used in the matrix scroller.
+
+def create_task(*args):
+    pass
+
+
+def sleep(*args):
+    pass
+
+
+def get_event_loop():
+    pass
+
+
+def run_forever():
+    pass
